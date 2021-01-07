537ceac0de4ab6977b70521a9d0e460ee7c6b7a9
tenachous <gabriel_oliveira_f_sousa@outlook.com>
2021-01-07 13:00:00 -0300

    Utilizando o onmouseover

---
+++ scriptImage.js
@@ -0,0 +1,94 @@
+//imagem para santo
+function functionImagemNossaSenhora(){ 
+    var img = new Image();
+    img.src="https://cdnv2.moovin.com.br/sjo/imagens/produtos/lista/medalha-milagrosa-nossa-senhora-das-gracas-dourado-2.jpg"; 
+    document.getElementById("imgSanto").src = img.src;
+}
+
+function functionImagemSantasCatolicas(){ 
+    var img = new Image();
+    img.src="https://cdnv2.moovin.com.br/sjo/imagens/produtos/lista/medalha-de-aluminio-santa-terezinha-dourada.jpg"; 
+    document.getElementById("imgSanto").src = img.src;
+}
+
+function functionImagemSantosCatolicos(){ 
+    var img = new Image();
+    img.src="https://cdnv2.moovin.com.br/sjo/imagens/produtos/lista/chaveiro-sao-bento-de-bronze-charo33.jpg"; 
+    document.getElementById("imgSanto").src = img.src;
+}
+
+//imagem para casa
+
+function functionImagemLivros(){
+    var img = new Image();
+    img.src="https://cdnv2.moovin.com.br/sjo/imagens/produtos/lista/livro-sjo-caYpia.jpg"; 
+    document.getElementById("imgCasa").src = img.src;
+}
+
+function functionImagemVela(){
+    var img = new Image();
+    img.src="https://cdnv2.moovin.com.br/sjo/imagens/produtos/lista/vela-para-batismo-1.jpg"; 
+    document.getElementById("imgCasa").src = img.src;
+}
+
+function functionImagemQuadro(){
+    var img = new Image();
+    img.src="https://cdnv2.moovin.com.br/sjo/imagens/produtos/lista/quadro-jesusmisericordioso-52cmx42cm-001.jpg"; 
+    document.getElementById("imgCasa").src = img.src;
+}
+
+
+function functionImagemPortVela(){
+    var img = new Image();
+    img.src="https://cdnv2.moovin.com.br/sjo/imagens/produtos/lista/porta-vela-de-vidro-transparente.jpg"; 
+    document.getElementById("imgCasa").src = img.src;
+}
+
+function functionImagemPortaRetrato(){
+    var img = new Image();
+    img.src="https://cdnv2.moovin.com.br/sjo/imagens/produtos/lista/porta-retrato-divino-espirito-santo-pmb110.png"; 
+    document.getElementById("imgCasa").src = img.src;
+}
+
+function functionImagemPortaChave(){
+    var img = new Image();
+    img.src="https://cdnv2.moovin.com.br/sjo/imagens/produtos/lista/porta-chave-sayo-bento.png"; 
+    document.getElementById("imgCasa").src = img.src;
+}
+
+function functionImagemPortaBiblia(){
+    var img = new Image();
+    img.src="https://cdnv2.moovin.com.br/sjo/imagens/produtos/lista/porta-biblia-de-mdf-escrito-jesus-30-cm-2.jpg"; 
+    document.getElementById("imgCasa").src = img.src;
+}
+
+function functionImagemImagens(){
+    var img = new Image();
+    img.src="https://cdnv2.moovin.com.br/sjo/imagens/produtos/lista/np3168.jpg"; 
+    document.getElementById("imgCasa").src = img.src;
+}
+
+function functionImagemCrucifixos(){
+    var img = new Image();
+    img.src="https://cdnv2.moovin.com.br/sjo/imagens/produtos/lista/sg15-1.jpg"; 
+    document.getElementById("imgCasa").src = img.src;
+}
+
+function functionImagemCapela(){
+    var img = new Image();
+    img.src="https://cdnv2.moovin.com.br/sjo/imagens/produtos/original/capela-de-madeira-02-3.png"; 
+    document.getElementById("imgCasa").src = img.src;
+}
+
+function functionImagemBibliaCatolica(){
+    var img = new Image();
+    img.src="https://cdnv2.moovin.com.br/sjo/imagens/produtos/original/9788527616096_1-1.jpeg"; 
+    document.getElementById("imgCasa").src = img.src;
+}
+
+
+function functionImagemAdorno(){
+    var img = new Image();
+    img.src="https://cdnv2.moovin.com.br/sjo/imagens/produtos/original/eder_brazolin-3_opt.jpg"; 
+    document.getElementById("imgCasa").src = img.src;
+}
